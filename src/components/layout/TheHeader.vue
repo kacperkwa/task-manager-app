1327c0655649d3256d4306d97f5584433d8c0984
kacperkwa <kacperkwartaxz@gmail.com>
2024-10-14 18:30:09 +0200

    testing src of img

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -18,13 +18,15 @@ import ArrowButton from '../UI/ArrowButton.vue';
 import DottedButton from '../UI/DottedButton.vue';
 import AddButton from '../UI/AddButton.vue';
 
-const logoSrc = ref('src/assets/img/logo-taskmate-bulb.png');
+const logoSrc = ref('');
 const updateLogo = () => {
 	const windowWidth = window.innerWidth;
 	if (windowWidth < 768) {
 		logoSrc.value = 'src/assets/img/logo-taskmate-bulb.png';
+		console.log(logoSrc.value)
 	} else {
 		logoSrc.value = 'src/assets/img/logo-taskmate.png';
+		console.log(logoSrc.value)
 	}
 };
 
