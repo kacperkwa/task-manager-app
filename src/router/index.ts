333f2b3ea2134f622453620ed96c3c65635e2120
kacperkwa <kacperkwartaxz@gmail.com>
2024-12-10 18:58:36 +0100

    fixed some issues with building page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,76 +7,76 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    meta: {
-      requiresAuth: true
-    }
-  }
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login
+	},
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+		meta: {
+			requiresAuth: true
+		}
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: Dashboard,
+		meta: {
+			requiresAuth: true
+		}
+	}
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_BASE_URL || '/'),
-  routes
+	history: createWebHistory(import.meta.env.VITE_APP_BASE_URL || '/'),
+	routes
 });
 
-router.beforeEach(async (to, from, next) => {
-  const userStore = useUserStore();
+router.beforeEach(async (to, _, next) => {
+	const userStore = useUserStore();
 
-  const user = await new Promise(resolve => {
-    onAuthStateChanged(auth, user => {
-      if (user) {
-        userStore.user = {
-          id: user.uid,
-          email: user.email || '',
-          userName: user.displayName || ''
-        };
-        userStore.isLoggedIn = true;
-      } else {
-        userStore.user = null;
-        userStore.isLoggedIn = false;
-      }
-      resolve(user);
-    });
-  });
-
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!userStore.isLoggedIn) {
-      next({ name: 'Login' });
-    } else {
-      if (to.name === 'Home') {
-        const hasBoard = await userStore.checkUserHasBoard();
-        if (hasBoard) {
-          next({ name: 'Dashboard' });
-        } else {
-          next();
-        }
-      } else {
-        next();
-      }
-    }
-  } else {
-    if (to.name === 'Login' && userStore.isLoggedIn) {
-      next({ name: 'Home' });
-    } else {
-      next();
-    }
-  }
+	const user = await new Promise(resolve => {
+		onAuthStateChanged(auth, user => {
+			if (user) {
+				userStore.user = {
+					id: user.uid,
+					email: user.email || '',
+					userName: user.displayName || ''
+				};
+				userStore.isLoggedIn = true;
+			} else {
+				userStore.user = null;
+				userStore.isLoggedIn = false;
+			}
+			resolve(user);
+		});
+	});
+	console.log(user);
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+		if (!userStore.isLoggedIn) {
+			next({ name: 'Login' });
+		} else {
+			if (to.name === 'Home') {
+				const hasBoard = await userStore.checkUserHasBoard();
+				if (hasBoard) {
+					next({ name: 'Dashboard' });
+				} else {
+					next();
+				}
+			} else {
+				next();
+			}
+		}
+	} else {
+		if (to.name === 'Login' && userStore.isLoggedIn) {
+			next({ name: 'Home' });
+		} else {
+			next();
+		}
+	}
 });
 
 export default router;
